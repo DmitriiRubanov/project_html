c979ef144533c15c76ee698eb5f414d38a804f25
secret <dmitrii.rubanov@yandex.ru>
2021-04-18 15:28:13 +0300

    регистрация

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from forms.loginform import LoginForm
+from forms.users import RegisterForm
+from data import db_session
+from data.users import User
+from flask_login import LoginManager, login_user
 
+db_session.global_init("db/blogs.db")
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
 def start():
     return render_template('start.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(name=form.name.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login')
+def login():
+    pass
 
 
 if __name__ == '__main__':
