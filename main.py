d01329b1e048974acfd7c69781c6934e63b674ba
secret <dmitrii.rubanov@yandex.ru>
2021-04-18 17:59:46 +0300

    авторизация

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from forms.loginform import LoginForm
 from forms.users import RegisterForm
 from data import db_session
 from data.users import User
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 db_session.global_init("db/blogs.db")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
@@ -36,9 +38,38 @@ def register():
     return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/product_menu")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/product_menu')
+def shop():
+    return """<a href="/logout" type="button" class="btn btn-outline-primary"
+     style="position: absolute; left: 783px; top: 387px; width: 210px">Выйти</a>"""
 
 
 if __name__ == '__main__':
